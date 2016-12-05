b5d12c7e869160f83f7ac881afed72ee0c5b925f
laurenkleach <lkleach@terpmail.umd.edu>
2016-12-05 00:24:16 -0500

    updated lambda function

--- Question.java
+++ Question.java
@@ -3,7 +3,7 @@ public class Question {
 
 	String name; // Idk if we need this if we just keep the order the same
 	int value;
-	int placeHolder;
+	BiFunction<Integer, Integer, Integer> weightFunction;
 	
 	/* Questions I'm starting with:
 	 * Yes or no questions
@@ -13,19 +13,21 @@ public class Question {
 		-Will you often study in the room?
 	 */
 	
-	public Question() {
+	public Question() { //do we need this?
 		this.name = "default";
 		value = -1;
 	}
 	
-	public Question(String qName, int val, int placeHolderForLambdaFunction) {
+	public Question(String qName, int val, BiFunction<Integer, Integer, Integer> weightFunction) {
 		this.name = qName;
 		this.value = val;
-		this.placeHolder = placeHolderForLambdaFunction; // TODO
+		this.weightFunction = weightFunction; // TODO
 	}
 	
-	public int calculate_weight() {
-		
+	public int calculate_weight(Answer answer1, Answer answer2) {
+		//return weightFunction.apply(answer1, answer2);
+		//Are we going to have answer object? Will the  
+		//int value of an answer object be stored as a field?
 		return 0;
 	}
 	
